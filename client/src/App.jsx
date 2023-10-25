2cb1e544eb7460bec6c741ae133195f049cf6bf0
apoorvapendse <apoorvavpendse@gmail.com>
2023-10-25 11:47:51 +0530

    screen sharing feature, no audio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { Peer } from 'peerjs';
 
@@ -7,7 +7,12 @@ function App() {
   const [myPeer, setMyPeer] = useState(null);
   const [receiverID, setReceiverID] = useState('');
   const myVideo = useRef(null);
-  const friendVideo = useRef(null);
+  const screenVideo = useRef(null);
+  const friendVideo = useRef(null); // Add a third video element for friend's video
+  const cameraStream = useRef(null);
+  const screenStream = useRef(null);
+  const friendStream = useRef(null);
+  const isSharingScreen = useRef(false); // Check if screen is being shared
 
   useEffect(() => {
     const peer = new Peer();
@@ -16,12 +21,15 @@ function App() {
 
     peer.on('open', function (id) {
       setMyPeerID(id);
+      console.log(id)
     });
 
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then(function (stream) {
+        cameraStream.current = stream;
         myVideo.current.srcObject = stream;
+        myVideo.current.muted = true;
         myVideo.current.play();
       })
       .catch(function (error) {
@@ -31,25 +39,60 @@ function App() {
     peer.on('call', function (call) {
       navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(
         function (stream) {
-          call.answer(stream);
+          call.answer(isSharingScreen.current ? screenStream.current : stream); // Answer with the shared screen or camera stream
           call.on('stream', function (remoteStream) {
+            friendStream.current = remoteStream;
             friendVideo.current.srcObject = remoteStream;
             friendVideo.current.play();
           });
         },
         function (err) {
-          console.log('Failed to get local stream', err);
+          console.log('Failed to get the local stream', err);
         }
       );
     });
   }, []);
-  console.log(myPeerID)
+
+  // Function to share screen
+  function shareScreen() {
+    if (!isSharingScreen.current) {
+      navigator.mediaDevices
+        .getDisplayMedia({ video: { cursor: 'always' } })
+        .then(function (stream) {
+          screenStream.current = stream;
+          screenVideo.current.srcObject = stream;
+          screenVideo.current.play();
+          isSharingScreen.current = true;
+          // Share the screen with the friend as well
+          const call = myPeer.call(receiverID, screenStream.current);
+          call.on('stream', function (remoteStream) {
+            friendStream.current = remoteStream;
+            friendVideo.current.srcObject = new MediaStream([screenStream.current, cameraStream.current]);
+            friendVideo.current.play();
+          });
+        })
+        .catch(function (error) {
+          console.error('Error accessing the screen:', error);
+        });
+    } else {
+      // Stop sharing screen
+      isSharingScreen.current = false;
+      screenStream.current.getTracks().forEach(track => track.stop());
+      screenVideo.current.srcObject = null;
+      // Resume using the camera stream in your video element
+      myVideo.current.srcObject = cameraStream.current;
+      myVideo.current.muted = true;
+    }
+  }
+
+  // Function to call the receiver
   function callTheReceiver() {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then(function (stream) {
-        let call = myPeer.call(receiverID, stream);
+        const call = myPeer.call(receiverID, isSharingScreen.current ? screenStream.current : stream);
         call.on('stream', function (remoteStream) {
+          friendStream.current = remoteStream;
           friendVideo.current.srcObject = remoteStream;
           friendVideo.current.play();
         });
@@ -59,8 +102,9 @@ function App() {
   return (
     <div>
       <div>
-        <video style={{ width: '400px', height: '400px', border: '1px solid' }} ref={myVideo}></video>
-        <video style={{ width: '400px', height: '400px', border: '1px solid' }} ref={friendVideo}></video>
+        <video style={{ width: '15vw', height: '50vh', border: '' }} ref={myVideo}></video>
+        <video style={{ width: '83vw', height: '90vh', border: '' }} ref={friendVideo}></video>
+        <video style={{ width: '100vw', height: '45vh', border: '', display: "none" }} ref={screenVideo}></video>
       </div>
       <input
         type="text"
@@ -69,6 +113,7 @@ function App() {
         onChange={(e) => setReceiverID(e.target.value)}
       />
       <button onClick={callTheReceiver}>Call</button>
+      <button onClick={shareScreen}>{isSharingScreen.current ? 'Stop Sharing' : 'Share Screen'}</button>
     </div>
   );
 }
